5e68180efe7f689ccf10ec80a3554f0146cf72a7
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-03-24 12:43:11 +0200

    [UPDATE] Writing your first Django app, part 5 (Testing Part 2)

--- polls/tests.py
+++ polls/tests.py
@@ -10,7 +10,6 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """was_published_recently() returns False
          for questions whose pub_date is in the future."""
@@ -37,12 +36,11 @@ def create_question(question_text, days):
        """Create a question with the given `question_text` and published the
        given number of `days` offset to now (negative for questions published
        in the past, positive for questions that have yet to be published)."""
-
        time = timezone.now() + datetime.timedelta(days=days)
        return Question.objects.create(question_text=question_text, pub_date=time)
 
-class QuestionIndexViewTests(TestCase):
 
+class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """If no questions exist, an appropriate message is displayed."""
         response = self.client.get(reverse('polls:index'))
@@ -87,3 +85,21 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """The detail view of a question with a pub_date in the future
+        returns a 404 not found."""
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """The detail view of a question with a pub_date in the past
+        displays the question's text."""
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
